1c3d46f7df26ee40fcd7afbd9ecc99fafa2adb28
yujin-K <lulu5736@gmail.com>
2019-08-18 22:14:38 +0900

    소식 바꿈

--- app/src/main/java/com/example/nt_project02/MainActivity.java
+++ app/src/main/java/com/example/nt_project02/MainActivity.java
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getDrawable(R.drawable.home),
                         Color.parseColor(colors[0]))
                         //.selectedIcon(getResources().getDrawable(R.drawable.ic_sixth))
-                        .title("소식")
+                        .title("thtlr")
                         .badgeTitle("NTB")
                         .build()
         );
