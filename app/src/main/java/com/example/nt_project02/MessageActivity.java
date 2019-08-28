eed2d3ecdd2ea4d36a7528900999311f4e6fd721
June <khm11904@naver.com>
2019-08-28 13:20:20 +0900

    채팅방 추가,채팅방 기능 수정 중

--- app/src/main/java/com/example/nt_project02/MessageActivity.java
+++ app/src/main/java/com/example/nt_project02/MessageActivity.java
@@ -1,6 +1,10 @@
 package com.example.nt_project02;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,21 +13,32 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageActivity extends AppCompatActivity {
     private String destinationUid;
     private Button button;
     private EditText editText;
 
+
+
+    private String TAG="sw";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<ChatData> chatList;
+    private String nick="nick2";
     private FirebaseDatabase database;
     private DatabaseReference myRef;
-    private DatabaseReference mDatabase;
-
 
 
 
@@ -35,42 +50,92 @@ public class MessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView );
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        chatList=new ArrayList<>();
+        mAdapter = new MessageAdapter(chatList,MessageActivity.this,nick);
+        recyclerView.setAdapter(mAdapter);
+
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+
+    /*    ChatData chat=new ChatData();
+        chat.setNickname(nick);
+        chat.setMsg("hi");
+        myRef.setValue(chat);*/
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                ChatData chat=dataSnapshot.getValue(ChatData.class);
+                ((MessageAdapter) mAdapter).addChat(chat);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
        // destinationUid=getIntent().getStringExtra("destinationUid");
         button=(Button)findViewById(R.id.messageActivity_Button);
         editText=(EditText)findViewById(R.id.messageActivity_editText);
 
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String msg=editText.getText().toString();
 
-                database = FirebaseDatabase.getInstance();
-                myRef = database.getReference("message");
+                if (msg != null) {
+                    ChatData chat=new ChatData();
+                    chat.setNickname(nick);
+                    chat.setMsg(msg);
+                    myRef.push().setValue(chat);
 
-                mDatabase = FirebaseDatabase.getInstance().getReference();
-
-                ChatModel chatModel=new ChatModel();
-                chatModel.uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
-                //chatModel.destinationUid=destinationUid;
-
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-                        String value = dataSnapshot.getValue(String.class);
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        // Failed to read value
-                    }
-                });
-
-
-                mDatabase.child("chatrooms").push().setValue(chatModel);
+                }
 
             }
         });
+
+
+
+
+
+
+
+
     }
 }
