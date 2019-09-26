4aba78bbfb11dd20abfc55c821af69f08b5efdc2
June <khm11904@naver.com>
2019-09-26 20:23:01 +0900

    폴더 정리3

---
+++ app/src/main/java/com/example/nt_project02/Fragment/News_Fragment.java
@@ -0,0 +1,23 @@
+package com.example.nt_project02.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.nt_project02.R;
+
+public class News_Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView=(ViewGroup) inflater.inflate(R.layout.news,container,false);
+
+        return rootView;
+    }
+}
