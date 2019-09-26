dd3fb883dbfab1127d8addd1d0189a9e54f7b0ac
Seonghui <seonghui3472@gmail.com>
2019-09-27 03:39:55 +0900

    image upload(9/27)

--- app/src/main/java/com/example/nt_project02/Fragment/Setting_Fragment.java
+++ app/src/main/java/com/example/nt_project02/Fragment/Setting_Fragment.java
@@ -1,6 +1,8 @@
 package com.example.nt_project02.Fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,6 +16,8 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.nt_project02.Native_Register;
@@ -31,16 +35,31 @@ import java.io.IOException;
 public class Setting_Fragment extends Fragment {
 
     ImageView ivUser;
-    private StorageReference mStorageRef;
-    Bitmap bitmap;
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
         ViewGroup rootView=(ViewGroup) inflater.inflate(R.layout.setting,container,false);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
 
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+
+
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+
+            }
+        }
 
         Button native_register=(Button) rootView.findViewById(R.id.native_register);
         native_register.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +74,7 @@ public class Setting_Fragment extends Fragment {
         ivUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View rootView) {
+
                 Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i,1);
             }
@@ -64,57 +84,46 @@ public class Setting_Fragment extends Fragment {
 
     }
 
-    public void uploadImage(){
-        StorageReference mountainsRef = mStorageRef.child("users").child("email"+".jpg");
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream ();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        byte[] data = baos.toByteArray ();
-
-        UploadTask uploadTask = mountainsRef.putBytes(data);
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle unsuccessful uploads
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @SuppressWarnings ( "VisibleForTests" )
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                //수정Uri downloadUrl = taskSnapshot.getDownloadUrl ();
-                //수정Log.d("url", String.valueOf(downloadUrl));
-
-            }
-
-        });
-
-
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Uri image = data.getData();
-        try{
-            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), image);
-            ivUser.setImageBitmap (bitmap);
-            uploadImage ();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     private void MystartActivity(Class c){
         Intent intent=new Intent(getActivity(),c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
 
-
-
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri image = data.getData();
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), image);
+            ivUser.setImageBitmap(bitmap);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+
+
+        }
+    }
 }
 
