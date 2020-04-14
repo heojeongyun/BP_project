d045c10ec1cd58a3ee37008f332f19cb182525be
june <khm11904@naver.com>
2020-04-14 11:02:50 +0900

    June_20200414_A

--- app/src/main/java/com/example/nt_project02/GoogleMap_Fragment.java
+++ app/src/main/java/com/example/nt_project02/GoogleMap_Fragment.java
@@ -1,11 +1,14 @@
 package com.example.nt_project02;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,15 +30,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoResponse;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -46,11 +52,20 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPhotoResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.Autocomplete;
 import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
 import java.util.Arrays;
@@ -60,7 +75,7 @@ import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 
-public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnInfoWindowClickListener {
 
 
     private static final LatLng DEFAULT_LOCATION=new LatLng(37.56,126.97);
@@ -70,6 +85,7 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
     private static final int UPDATE_INTERVAL_MS=15000;
     private static final int FASTEST_UPDATE_INTERVAL_MS=15000;
     private static final int AUTOCOMPLETE_REQUEST_CODE =1 ;
+    private static final int REQUEST_CONTENT = 101;
 
     private GoogleMap mMap=null;
     private MapView mapView = null;
@@ -78,6 +94,10 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
     private LatLng s;
     private Place place;
     private Location location=new Location("");
+    private String Content;
+    private PlacesClient placesClient;
+    private Bitmap bitmap;
+
 
 
 
@@ -94,14 +114,18 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
 
     }
 
-    public void setCurrentLocation(Location location, String markerTitle, String markerSnippet){
+    //파리미터 위치로 마크를 찍고 카메라 이동해주는 메서드
+    public void setCurrentLocation(Location location, String markerTitle, String markerSnippet, String Content, Bitmap bitmap){
         if( currentMarker !=null)
             currentMarker.remove();
 
         if(location !=null){
             //현재위치의 위도 경도 가져옴
+
+            
             LatLng currentLocation =new LatLng(location.getLatitude(),location.getLongitude());
 
+            //마커 설정
             MarkerOptions markerOptions=new MarkerOptions();
             markerOptions.position(currentLocation);
             markerOptions.title(markerTitle);
@@ -110,6 +134,12 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
             markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
             currentMarker=mMap.addMarker(markerOptions);
 
+            //CustomWindow 설정
+            View infoWindow = getLayoutInflater().inflate(R.layout.googlemap_custom_infowindow, null);
+            CustomInfoAdapter customInfoAdapter = new CustomInfoAdapter(infoWindow, markerTitle,markerSnippet, Content, bitmap);
+            mMap.setInfoWindowAdapter(customInfoAdapter);
+
+            //지도 카메라 이동
             mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
             mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
             return;
@@ -133,14 +163,22 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
         mapView = (MapView)layout.findViewById(R.id.map);
         mapView.getMapAsync(this);
 
+
+        //Google Places Api 초기 설튼 설정
+
         Places.initialize(getContext(), "AIzaSyA7YlrC4J7fuD4VCTkCNgoQzg8zTVJytsg");
-        PlacesClient placesClient = Places.createClient(getContext());
+        placesClient = Places.createClient(getContext());
 
         Button button=(Button) layout.findViewById(R.id.button);
 
 
 
+
+        //검색 버튼
         button.setOnClickListener(new View.OnClickListener() {
+
+
+
             @Override
             public void onClick(View v) {
 
@@ -150,8 +188,11 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
 
 
 
+
+
+
                 // Set the fields to specify which types of place data to return.
-                List<Place.Field> fields = Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME, Place.Field.ADDRESS,Place.Field.ID);
+                List<Place.Field> fields = Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME, Place.Field.ADDRESS,Place.Field.ID,Place.Field.PHOTO_METADATAS);
 
                 // Start the autocomplete intent.
                 Intent intent = new Autocomplete.IntentBuilder(
@@ -234,14 +275,58 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
 
                 place = Autocomplete.getPlaceFromIntent(data);
 
-
                 location.setLatitude(place.getLatLng().latitude);
                 location.setLongitude(place.getLatLng().longitude);
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId()+ ", "+place.getLatLng());
 
 
+                if(place.getPhotoMetadatas() !=null) {
+                    PhotoMetadata photoMetadata = place.getPhotoMetadatas().get(0);
 
-                setCurrentLocation(location,place.getName(),place.getAddress());
+
+                    // Create a FetchPhotoRequest.
+                    FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(photoMetadata)
+                            .setMaxWidth(500) // Optional.
+                            .setMaxHeight(300) // Optional.
+                            .build();
+
+                    placesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
+                        @Override
+                        public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
+
+
+                            bitmap = fetchPhotoResponse.getBitmap();
+                            //검색 된 위치 설정
+                            setCurrentLocation(location, place.getName(), place.getAddress(), "장소를 설명해주세요!", bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            if (exception instanceof ApiException) {
+                                ApiException apiException = (ApiException) exception;
+                                int statusCode = apiException.getStatusCode();
+                                // Handle error with given status code.
+                                Log.e("PlacesAPI", "Place not found: " + exception.getMessage());
+                            }
+                        }
+                    });
+
+                    //장소 사진 정보가 없을 때
+                }else{
+                    bitmap= BitmapFactory.decodeResource(getContext().getResources(), R.drawable.bluepeople_logo);
+                    setCurrentLocation(location, place.getName(), place.getAddress(), "장소를 설명해주세요!", bitmap);
+                }
+
+
+
+
+
+
+
+
+
+
+
 
 
             } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
@@ -251,6 +336,18 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
             } else if (resultCode == RESULT_CANCELED) {
                 // The user canceled the operation.
             }
+
+        }
+
+        if(requestCode == REQUEST_CONTENT){ //구글 맵 InfoWindow 클릭 시
+            if(resultCode== RESULT_OK){ //결과가 정상적이라면
+
+
+                Content=data.getStringExtra("Content"); // InfoWindow_Edit Activity 데이터 가져오기
+                setCurrentLocation(location,place.getName(),place.getAddress(),Content,bitmap); //마커 정보 다시 설정
+                Log.d(TAG,"확인");
+            }
+
         }
     }
 
@@ -265,14 +362,19 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
         mMap=googleMap;
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
+        markerOptions.position(pknu);
         markerOptions.title("부경대");
         markerOptions.snippet("가온관");
         mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pknu));
 
         mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        mMap.setOnInfoWindowClickListener(this);
+
+
+
 
 
 
@@ -296,6 +398,22 @@ public class GoogleMap_Fragment extends Fragment implements OnMapReadyCallback, 
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    //Custom InfoWindow 클릭 시 마커정보 수정 액티비티로 이동
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(getContext(), "Info window clicked",Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(getContext(),InfoWindow_Edit.class);
+        intent.putExtra("Place_Name",place.getName());
+        intent.putExtra("Place_Adress",place.getAddress());
+        getActivity().startActivityForResult(intent,REQUEST_CONTENT);
+
+    }
+
+
+
+
+
 }
 
 
