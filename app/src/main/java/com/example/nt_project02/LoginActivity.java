717b7f41fdea80e63f8c3246df074508df82b3e8
heojeongyun <kelly981217@naver.com>
2020-05-05 20:33:46 +0900

    20200505_A

--- app/src/main/java/com/example/nt_project02/LoginActivity.java
+++ app/src/main/java/com/example/nt_project02/LoginActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,14 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
-import com.example.nt_project02.Chat.UserModel;
-import com.google.android.gms.auth.api.Auth;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
@@ -37,11 +33,11 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity  {
     private static final String TAG = "LoginActivity";
     private Context mContext;
     private FirebaseAuth mAuth; //파이어베이스 인증객체
-    private GoogleApiClient googleApiClient; //구글 API 클라이언트 객체
+    private GoogleSignInClient mGoogleSignInClient; //구글 API 클라이언트 객체
     private static final int REO_SIGN_GOOGLE = 100; //구글 로그인 결과 코드
     private SignInButton signInButton; //구글로그인 버튼
 
@@ -58,21 +54,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .requestEmail()
                 .build();
 
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-
-        // 초기화 Firebase Auth
-
-/*        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);*/
-
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
         findViewById(R.id.CheckButton).setOnClickListener(onClickListener);
@@ -84,10 +66,18 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient); //구글에서 제공하는 인증 액티비티 같은 것이 있음
-                startActivityForResult(intent, REO_SIGN_GOOGLE);
+                signIn();
             }
         });
+
+    }
+
+
+    private void signIn() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REO_SIGN_GOOGLE);
+
     }
 
     @Override
@@ -115,6 +105,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {//구글로그인이 성공했으면
+//                                    FirebaseUser user = mAuth.getCurrentUser();
                                     userLogin();
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -132,6 +123,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
             FirebaseUser user = mAuth.getCurrentUser();
             String uid = user.getUid();
+
             // 파이어스토어 객체선언
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             //파이어스토어에서 해당 유저의 uid를 이용하여 정보 가져오기
@@ -143,16 +135,13 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                     if (task.isSuccessful()) {
 
                         //DocumentSnapshot에 정보를 담아둠
-
                         DocumentSnapshot document = task.getResult();
 
                         //document가 null이 아닐 때
-
                         if (document != null) {
 
                             //재차 확인
                             if (document.exists()) {
-
 
                                 MystartActivity(MainActivity.class);
                             } else {
@@ -161,16 +150,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                                 MystartActivity(MemberActivity.class);
                             }
                         }
-
-/*                            if(유저모델에 있는 닉네임이 널이라면 멤버액티비티로 가게) {
-                                Intent intent = new Intent(getApplicationContext(), MemberActivity.class);
-                                intent.putExtra("email", account.getEmail());
-                                intent.putExtra("photourl", String.valueOf(account.getPhotoUrl()));//photoUrl을 String 형태로 바꿈
-                                startActivity(intent);
-                            } else {
-                                닉네임이 있다면 메인액티비티로 가게
-                                MystartActivity(MainActivity.class);
-                            }*/
                     } else { //로그인 실패했으면
                         Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
                     }
@@ -180,9 +159,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
 
     }
-
-
-
 
 
     public void onBackPressed() {
@@ -226,12 +202,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         }
 
 
-/*        private void signIn() {
-            
-            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-            startActivityForResult(signInIntent, RC_SIGN_IN);
 
-        }*/
 
 
     };
@@ -336,8 +307,4 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         startActivity(intent);
     }
 
-    @Override
-    public void onConnectionFailed (@NonNull ConnectionResult connectionResult){
-
-    }
 }
