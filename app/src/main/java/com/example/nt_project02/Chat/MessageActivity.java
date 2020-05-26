256867e69e0d23a947a6033daa8ca66ed600d57c
june <khm11904@naver.com>
2020-05-26 15:05:12 +0900

    June_20200526_B

--- app/src/main/java/com/example/nt_project02/Chat/MessageActivity.java
+++ app/src/main/java/com/example/nt_project02/Chat/MessageActivity.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -139,6 +140,7 @@ public class MessageActivity extends AppCompatActivity {
 
         Log.d(TAG,"deuid:"+destinationUid);
         Log.d(TAG,"uid:"+uid);
+
 
 
 
